bf11b43977bb487cbe3677ed797e303d90330ceb
artem schegortzov <tema.chegortzov@mail.ru>
2024-05-14 10:55:25 +0300

    done LeftMenu
    done LeftMenu main component for aside

--- frontend/src/layouts/components/menu/LeftMenu.jsx
+++ frontend/src/layouts/components/menu/LeftMenu.jsx
@@ -1,5 +1,16 @@
+import ButtonPage from './components/leftMenu/ButtonPage'
+
+import { ReactComponent as HomeSVG } from '../../../assets/svgs/home.svg'
+import { ReactComponent as GamepadSVG } from '../../../assets/svgs/gamepad.svg'
+import { ReactComponent as LibrarySVG } from '../../../assets/svgs/library.svg'
+import { ReactComponent as DiamondSVG } from '../../../assets/svgs/diamond.svg'
+import { ReactComponent as PackageSVG } from '../../../assets/svgs/package.svg'
+import { ReactComponent as PeopleSVG } from '../../../assets/svgs/people.svg'
+import { ReactComponent as HelpSVG } from '../../../assets/svgs/help.svg'
+
 import menuStyles from './Menu.module.scss'
 import styles from './LeftMenu.module.scss'
+import { Link } from 'react-router-dom'
 
 function LeftMenu({ className, ...params }) {
   return (
@@ -7,7 +18,28 @@ function LeftMenu({ className, ...params }) {
       {...params}
       className={[menuStyles.menu, styles.leftMenu, className].join(' ')}
     >
-      Левая часть с меню
+      <nav className={styles.nav}>
+        <ButtonPage name="Главная страница" svgComponent={<HomeSVG />} active />
+        <ButtonPage name="Магазин" svgComponent={<GamepadSVG />} />
+        <ButtonPage
+          name="Библиотека"
+          svgComponent={<LibrarySVG />}
+          notificationsCount={3}
+        />
+        <ButtonPage name="Инвентарь" svgComponent={<DiamondSVG />} />
+        <ButtonPage name="Торговая площадка" svgComponent={<PackageSVG />} />
+        <ButtonPage name="Друзья" svgComponent={<PeopleSVG />} />
+        <ButtonPage name="Помощь" svgComponent={<HelpSVG />} />
+      </nav>
+      <footer className={styles.footer}>
+        <Link to="/" className={styles.footer__link}>
+          Правовая информация
+        </Link>
+        <Link to="/" className={styles.footer__link}>
+          Политика конфиденциальности
+        </Link>
+        <span className={styles.footer__text}>©2024 Щегорцов А.М.</span>
+      </footer>
     </aside>
   )
 }
