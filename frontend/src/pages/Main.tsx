35baf4eb8afa8710a5ad348849d270ccaca4225c
artem schegortzov <tema.chegortzov@mail.ru>
2024-05-15 16:51:03 +0300

    updated Main page, added isNewGame for game card

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -5,6 +5,41 @@ import GameBigCard from '../components/cards/GameBigCard'
 
 import styles from './Main.module.scss'
 
+interface IGameBigCard {
+  name: string
+  imgSrc: string
+  aboutGame: string
+  to: string
+  newGame: boolean
+}
+
+const gameBigCards: IGameBigCard[] = [
+  {
+    name: 'FarCry Primal',
+    imgSrc: 'farcry/large.jpg',
+    aboutGame:
+      'Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev...',
+    to: '/',
+    newGame: false,
+  },
+  {
+    name: 'FarCry Primal',
+    imgSrc: 'farcry/large.jpg',
+    aboutGame:
+      'Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev...',
+    to: '/',
+    newGame: true,
+  },
+  {
+    name: 'FarCry Primal',
+    imgSrc: 'farcry/large.jpg',
+    aboutGame:
+      'Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev...',
+    to: '/',
+    newGame: false,
+  },
+]
+
 interface MainProps {
   addNewFloatingPanel?: () => void
   setPageName: (name: string) => void
@@ -35,72 +70,15 @@ function Main({ addNewFloatingPanel = () => {}, setPageName }: MainProps) {
           </div>
           <div className={styles.section__contentWrapper}>
             <div className={styles.section__contnet}>
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
-              <GameBigCard
-                name="FarCry Primal"
-                imgSrc="farcry/large.jpg"
-                aboutGame="Компьютерная игра в жанре action-adventure со структурой открытого мира, разработанная студией Ubisoft Montreal при поддержке Ubisoft Toronto, Ubisoft Kiev..."
-                to="/"
-              />
+              {gameBigCards.map((gameBigCard) => (
+                <GameBigCard
+                  name={gameBigCard.name}
+                  imgSrc={gameBigCard.imgSrc}
+                  aboutGame={gameBigCard.aboutGame}
+                  to={gameBigCard.to}
+                  newGame={gameBigCard.newGame}
+                />
+              ))}
             </div>
           </div>
         </section>
