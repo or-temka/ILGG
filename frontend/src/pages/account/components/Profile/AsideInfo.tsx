970e4013d63c88cfe2536dfeb0775f5721edf283
artem schegortzov <tema.chegortzov@mail.ru>
2024-05-21 14:04:47 +0300

    some changes

--- frontend/src/pages/account/components/Profile/AsideInfo.tsx
+++ frontend/src/pages/account/components/Profile/AsideInfo.tsx
@@ -1,7 +1,12 @@
 import styles from './AsideInfo.module.scss'
+import ProfileBlockedPanel from './ProfileBlockedPanel'
 
 function AsideInfo() {
-  return <div className={styles.aside}></div>
+  return (
+    <div className={['asideContent', styles.aside].join(' ')}>
+      <ProfileBlockedPanel />
+    </div>
+  )
 }
 
 export default AsideInfo
