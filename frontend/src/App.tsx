d0696148a1e8d7340e8d6a5486d4156f099bb00e
artem schegortzov <tema.chegortzov@mail.ru>
2024-05-21 22:15:37 +0300

    some changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
-import { useMemo } from 'react'
+import { lazy, useMemo } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import MainLayout from './layouts/MainLayout'
-import Main from './pages/Main'
-import SignUp from './pages/account/SignUp'
-import Profile from './pages/account/Profile'
-import PageNotFound from './pages/PageNotFound'
 
 import FloatingPanelsQueue from './components/UI/floatingPanels/FloatingPanelsQueue'
 import pageLink from './pagesLinks'
 
 import useInitialization from './initialize'
+
+const Main = lazy(() => import('./pages/Main'))
+const SignUp = lazy(() => import('./pages/account/SignUp'))
+const Profile = lazy(() => import('./pages/account/Profile'))
+const PageNotFound = lazy(() => import('./pages/PageNotFound'))
 
 function App() {
   // init all data
