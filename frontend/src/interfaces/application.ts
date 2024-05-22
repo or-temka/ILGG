ca85c7dd02c2f5b45349e66476b362f75bc3e0a5
artem schegortzov <tema.chegortzov@mail.ru>
2024-05-22 20:54:52 +0300

    some changes

--- frontend/src/interfaces/application.ts
+++ frontend/src/interfaces/application.ts
@@ -12,9 +12,10 @@ export interface IApplication extends ISimpleApplication {}
 
 export interface IShopApplication extends ISimpleApplication {
   developer: string
-  type: MongoId
-  genres: MongoId[]
+  types: MongoId[]
   categories: MongoId[]
+  themes: MongoId[]
+  playerTypes: MongoId[]
 }
 
 // categories
@@ -23,14 +24,20 @@ export type AppCategory = {
   name: string
 }
 
-// genre
-export type AppGenre = {
+// app types
+export type AppType = {
   _id: MongoId
   name: string
 }
 
-// app types
-export type AppType = {
+// app themes
+export type AppTheme = {
+  _id: MongoId
+  name: string
+}
+
+// app player type
+export type AppPlayerType = {
   _id: MongoId
   name: string
 }
