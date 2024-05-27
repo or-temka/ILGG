2053859b1fa18ffa9738a0f2375b683654887eb9
artem schegortzov <tema.chegortzov@mail.ru>
2024-05-27 09:16:28 +0300

    done Header for Game page
    done Header component for Game page

--- frontend/src/pages/games/components/Game/Header/Header.tsx
+++ frontend/src/pages/games/components/Game/Header/Header.tsx
@@ -7,24 +7,30 @@ import styles from './Header.module.scss'
 function Header() {
   return (
     <header className={styles.header}>
-      <div></div>
-      <div>
-        <div>
-          <h2>Find Number</h2>
+      <div className={[styles.header__content, styles.content].join(' ')}>
+        <div className={styles.content__header}>
+          <h2 className={styles.content__label}>Find Number</h2>
         </div>
-        <div>
-          <div>
+        <div className={styles.content__mainInfo}>
+          <div className={styles.content__photoLibrary}>
             <GamePhotoLibrary />
           </div>
-          <div>
-            <div>
+          <div className={styles.content__control}>
+            <div className={styles.content__aboutGame}>
               <HeaderAboutGame />
             </div>
-            <div>
+            <div className={styles.content__cotrolButtons}>
               <GameControlButtons />
             </div>
           </div>
         </div>
+      </div>
+      <div className={[styles.header__background, styles.background].join(' ')}>
+        <div className={styles.background__gradient} />
+        <img
+          src={require('../../../../../assets/images/applications/find-number/large-poster.jpg')}
+          className={styles.background__img}
+        />
       </div>
     </header>
   )
