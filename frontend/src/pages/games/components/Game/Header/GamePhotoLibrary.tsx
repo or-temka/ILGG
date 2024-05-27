be513250fa83d74d34c4d759968614ac59bceb34
artem schegortzov <tema.chegortzov@mail.ru>
2024-05-27 09:15:12 +0300

    added GamePhotoLibrary
    added GamePhotoLibrary game component of header

--- frontend/src/pages/games/components/Game/Header/GamePhotoLibrary.tsx
+++ frontend/src/pages/games/components/Game/Header/GamePhotoLibrary.tsx
@@ -3,7 +3,11 @@ import styles from './GamePhotoLibrary.module.scss'
 function GamePhotoLibrary() {
   return (
     <div className={styles.photos}>
-      <img src="" alt="" />
+      <img
+        src={require('../../../../../assets/images/applications/find-number/large-poster.jpg')}
+        alt="Изображение игры"
+        className={styles.photos__photo}
+      />
     </div>
   )
 }
