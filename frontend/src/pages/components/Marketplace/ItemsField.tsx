e57f624cbee1f25c687bbc39785324b75ea81814
artem schegortzov <tema.chegortzov@mail.ru>
2024-06-01 20:58:54 +0300

    done marketplace

---
+++ frontend/src/pages/components/Marketplace/ItemsField.tsx
@@ -0,0 +1,107 @@
+import Button, { ButtonVariant } from '../../../components/UI/buttons/Button'
+import styles from './ItemsField.module.scss'
+
+interface ItemsFieldProps {
+  classNames?: {
+    wrapper?: string
+  }
+}
+
+function ItemsField({ classNames }: ItemsFieldProps) {
+  return (
+    <section className={[styles.items, classNames?.wrapper].join(' ')}>
+      <div className={styles.items__itemsBtns}>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+        <button className={styles.item}>
+          <img
+            src={require('../../../assets/images/applications/find-number/items/1.png')}
+            alt="Название предмета"
+            className={styles.item__img}
+          />
+        </button>
+      </div>
+
+      <div className={styles.items__pagination}>
+        <div className={styles.pagination}>
+          <Button
+            title="Назад"
+            variant={ButtonVariant.light}
+            className={[styles.pagination__btn].join(' ')}
+          />
+          <span className={styles.pagination__pageText}>
+            <span className={styles.pagination__nowPageNum}>1</span> / 12
+          </span>
+          <Button
+            title="Далее"
+            variant={ButtonVariant.light}
+            className={[styles.pagination__btn].join(' ')}
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ItemsField
