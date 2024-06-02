21e1af30a33fab4096705d3349eb536dc6f13703
artem schegortzov <tema.chegortzov@mail.ru>
2024-06-02 11:15:54 +0300

    Update serverLog.ts

--- backend/src/utils/serverLog.ts
+++ backend/src/utils/serverLog.ts
@@ -1,13 +1,19 @@
-import clc from 'cli-colors'
-
 export const serverLog = (text: string) => {
-  console.log(clc.green('[msg]') + ' ' + text)
+  console.log(`\u001b[32m[msg]\u001b[0m ${text}`)
 }
 
 export const serverError = (text: string) => {
-  console.log(clc.red('[error]') + ' ' + text)
+  console.log(`\u001b[31m[error]\u001b[0m ${text}`)
+}
+
+export const serverFatalError = (text: string) => {
+  console.log(`\u001b[41m[error]\u001b[0m \u001b[31m${text}\u001b[0m`)
 }
 
 export const serverMsg = (text: string) => {
-  console.log(clc.green(text))
+  console.log(`\u001b[32m[msg] ${text}\u001b[0m`)
+}
+
+export const serverHint = (text: string) => {
+  console.log(`\u001b[30m[hint] ${text}\u001b[0m`)
 }
