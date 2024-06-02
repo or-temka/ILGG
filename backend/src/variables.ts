00b22ff85960016ba3f9c454274da140e74e8432
artem schegortzov <tema.chegortzov@mail.ru>
2024-06-02 15:15:10 +0300

    some changes

--- backend/src/variables.ts
+++ backend/src/variables.ts
@@ -1,2 +1,4 @@
-export const DB_NAME = "ilgg"
-export const DB_CLUSTER_NAME = "cluster0"
+export const DB_NAME = 'ilgg'
+export const DB_CLUSTER_NAME = 'cluster0'
+
+export const BASE_API_URL = '/api'
